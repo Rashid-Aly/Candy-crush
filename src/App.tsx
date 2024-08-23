f42ee95dee0709643298ef0661db583baa070257
Rashid ali <96859861+Rashid-Aly@users.noreply.github.com>
2024-08-23 21:14:47 +0500

    update data
    update
    
    Co-Authored-By: muzamil hussain <91672496+muzamil077@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import {
 function App() {
   const dispatch = useAppDispatch();
   const board = useAppSelector(({ candyCrush: { board } }) => board);
+
   const boardSize = useAppSelector(
     ({ candyCrush: { boardSize } }) => boardSize
   );
@@ -25,7 +26,7 @@ function App() {
   useEffect(() => {
     dispatch(updateBoard(createBoard(boardSize)));
   }, [dispatch, boardSize]);
-
+console.log(dispatch, "dispatch")
   useEffect(() => {
     const timeout = setTimeout(() => {
       const newBoard = [...board];
